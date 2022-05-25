9239de35f7e3ebc9bded7de25db4f2ebcca771ff
gustavo <gustavohms1998@gmail.com>
2022-05-25 09:45:33 -0300

    react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Home.css';
+
+function Home(){
+    return(
+        <>
+            <h1 className="titulo">Home</h1>
+           <img src="https://i.redd.it/2b20ehxqtwz41.jpg" alt ="a" className="img"/>
+        </>
+    )
+}
+
+export default Home;
